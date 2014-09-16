e180c0a0d47a2063a2f91c777ae735660e7e0ef0
rocketpastsix <mjftrask@gmail.com>
2014-09-15 21:18:12 -0400

    Added base of project

---
+++ index.php
@@ -0,0 +1,70 @@
+<!doctype html>
+<html>
+	<head>	
+		<meta charset="utf-8">
+		<meta description="children dance jaz tap ballet plays kids obd music lessons voice mom and me roswell crabapple alpharetta milton">
+		<title>Off Broadway Dance Studio | Childrens Dance Studio in Roswell</title>
+
+		<!-- Fonts -->
+		<link href='http://fonts.googleapis.com/css?family=Lato:300,400,700' rel='stylesheet' type='text/css'>
+
+		<!-- Styles -->
+
+		<link rel="stylesheet" type="text/css" href="css/foundation.min.css">
+		<link rel="stylesheet" type="text/css" href="css/style.css">
+		<!-- Javacript CDN -->
+
+
+
+	</head>
+	<body>
+		<nav>
+
+
+
+		</nav>
+
+		<main id="mainbody">
+			<h1>Off Broadway Dance Studio</h1>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+		</main>
+
+		<footer>
+
+
+		</footer>
+	</body>
+</html>

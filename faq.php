5d66ae63526f94d9632d2fdc9c5024873396eef6
rocketpastsix <mjftrask@gmail.com>
2014-10-05 09:58:27 -0400

    updated jquery
    FAQ now had the jQuery in place to hide and show

--- faq.php
+++ faq.php
@@ -87,7 +87,7 @@
 		<div class="row">
 			<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
 				<h3 class="text-center click_me">Can parents stay with their student during class?</h3>
-				<p class="text-center" id="hide_div">Children's Theatre classes are an independent experience for children to learn and explore in a classroom enhancing their social, developmental, and creative skills in a peer setting. For Creative Drama classes, we provide an opportunity for parents to observe the class experience during Sneak-a-Peek as well as on the Sharing Day. Other than the Sneak-a-Peek and Sharing, parents do not sit in on classes. Although it is rare, some of our youngest students may experience separation anxiety on the first day of class. We find the most effective method for separation anxiety is a quick but loving goodbye from the parent. Our class assistant or another staff member will provide your child with the one on one attention required to sooth them until they are comfortable and engaged in the class. Once students begin class activities, anxiety typically diminishes. If you have further questions or concerns, do not hesitate to contact us.</p>
+				<p class="text-center hide_div">Children's Theatre classes are an independent experience for children to learn and explore in a classroom enhancing their social, developmental, and creative skills in a peer setting. For Creative Drama classes, we provide an opportunity for parents to observe the class experience during Sneak-a-Peek as well as on the Sharing Day. Other than the Sneak-a-Peek and Sharing, parents do not sit in on classes. Although it is rare, some of our youngest students may experience separation anxiety on the first day of class. We find the most effective method for separation anxiety is a quick but loving goodbye from the parent. Our class assistant or another staff member will provide your child with the one on one attention required to sooth them until they are comfortable and engaged in the class. Once students begin class activities, anxiety typically diminishes. If you have further questions or concerns, do not hesitate to contact us.</p>
 			</div>
 		</div>
 	</div>
@@ -95,8 +95,8 @@
 	<div class="container">
 		<div class="row">
 			<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-				<h3 class="text-center" id="click_me">A note about separation anxiety</h3>
-				<p class="text-center" id=" hide_div">Some of our youngest students may experience some separation anxiety on the first day of class. We find the most effective method for separation anxiety is a quick but loving goodbye from the parent. Once they arrive in the classroom, students are immediately engaged in classroom activities. It is our firm policy that parents do not accompany students into the classroom. We have found that this tends to heighten the distress of the student and makes the separation even more difficult. This also sets a very difficult precedent for the other students in the class who cannot understand why their parent is not with them as well. Parents are welcome to wait in the lobby area during class. Another strategy which helps to decrease anxiety is to be sure your student arrives on time, especially for the first day of class. Late students tend to feel more anxiety and have a more difficult time joining class then those who begin class with the entire group.</p>
+				<h3 class="text-center click_me">A note about separation anxiety</h3>
+				<p class="text-center hide_div">Some of our youngest students may experience some separation anxiety on the first day of class. We find the most effective method for separation anxiety is a quick but loving goodbye from the parent. Once they arrive in the classroom, students are immediately engaged in classroom activities. It is our firm policy that parents do not accompany students into the classroom. We have found that this tends to heighten the distress of the student and makes the separation even more difficult. This also sets a very difficult precedent for the other students in the class who cannot understand why their parent is not with them as well. Parents are welcome to wait in the lobby area during class. Another strategy which helps to decrease anxiety is to be sure your student arrives on time, especially for the first day of class. Late students tend to feel more anxiety and have a more difficult time joining class then those who begin class with the entire group.</p>
 			</div>
 		</div>
 	</div>
@@ -104,8 +104,8 @@
 	<div class="container">
 		<div class="row">
 			<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-				<h3 class="text-center" id="click_me">What happens in a typical class?</h3>
-				<p class="text-center" id="hide_div">Early Childhood classes typically begin with a group circle activity which joins the students together and sets the tone for creative learning. Students practice group sharing, where a prompt is used to inspire a short presentational statement. Students engage in a variety of physical and vocal warm up activities including pantomime and creative movement. Each class is theme based, and each week a new story aligns with the theme. The Teaching Artist reads the story to the students and then leads them through a dramatization of the story. Teaching Artists incorporate a variety of visual and tactile components as a creative exploration. Music and craft activities which correlate to the theme may also be incorporated to the class. Students receive a printed handout to take home which includes written activities and comprehension questions about the story explored in class that day. 
+				<h3 class="text-center click_me">What happens in a typical class?</h3>
+				<p class="text-center hide_div">Early Childhood classes typically begin with a group circle activity which joins the students together and sets the tone for creative learning. Students practice group sharing, where a prompt is used to inspire a short presentational statement. Students engage in a variety of physical and vocal warm up activities including pantomime and creative movement. Each class is theme based, and each week a new story aligns with the theme. The Teaching Artist reads the story to the students and then leads them through a dramatization of the story. Teaching Artists incorporate a variety of visual and tactile components as a creative exploration. Music and craft activities which correlate to the theme may also be incorporated to the class. Students receive a printed handout to take home which includes written activities and comprehension questions about the story explored in class that day. 
 									   Elementary, Middle, and High school classes begin with focused warm up activities and exercises. Depending on the nature of the class, students then engage in the exploration of improvisation, musical theatre or scene study activities. Core theatre skills such as projection, diction, blocking, stage directions and objectives are reinforced through practical application of rehearsing scenes and musical numbers.</p>
 			</div>
 		</div>
@@ -114,8 +114,8 @@
 	<div class="container">
 		<div class="row">
 			<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-				<h3 class="text-center" id="click_me">What is a School of Theatre Training Showcase?</h3>
-				<p class="text-center" id="hide_div">On the final class day, parents and friends are invited into the classroom for an informal performance demonstrating some of the work done throughout the year. Showcases often include performances of audition packages, scenes, musical numbers, and a certificate presentation.</p>
+				<h3 class="text-center click_me">What is a School of Theatre Training Showcase?</h3>
+				<p class="text-center hide_div">On the final class day, parents and friends are invited into the classroom for an informal performance demonstrating some of the work done throughout the year. Showcases often include performances of audition packages, scenes, musical numbers, and a certificate presentation.</p>
 			</div>
 		</div>
 	</div>
@@ -123,8 +123,8 @@
 	<div class="container">
 		<div class="row">
 			<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-				<h3 class="text-center" id="click_me">What if I need to switch classes/camps?</h3>
-				<p class="text-center" id="hide_div">If families find that they have a scheduling conflict and need to switch their class day or time or camp, they may call call or email us to request this change. Students will not be moved from their assigned class or camps after the refund deadline. Please be aware that switching class is contingent upon space availability in the desired class.</p>
+				<h3 class="text-center click_me">What if I need to switch classes/camps?</h3>
+				<p class="text-center hide_div">If families find that they have a scheduling conflict and need to switch their class day or time or camp, they may call call or email us to request this change. Students will not be moved from their assigned class or camps after the refund deadline. Please be aware that switching class is contingent upon space availability in the desired class.</p>
 			</div>
 		</div>
 	</div>
@@ -132,8 +132,8 @@
 	<div class="container">
 		<div class="row">
 			<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-				<h3 class="text-center" id="click_me">Is there a chance that a class could be cancelled due to low enrollment?</h3>
-				<p class="text-center" id="hide_div">Children's Theatre reserves the right to cancel classes for which fewer than five students are enrolled. In this event, we will notify the families affected by a cancelled class with a choice of suitable replacement classes and/or full refunds.</p>
+				<h3 class="text-center click_me">Is there a chance that a class could be cancelled due to low enrollment?</h3>
+				<p class="text-center hide_div">Children's Theatre reserves the right to cancel classes for which fewer than five students are enrolled. In this event, we will notify the families affected by a cancelled class with a choice of suitable replacement classes and/or full refunds.</p>
 			</div>
 		</div>
 	</div>
@@ -141,8 +141,8 @@
 	<div class="container">
 		<div class="row">
 			<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-				<h3 class="text-center" id="click_me">Are make up classes available?</h3>
-				<p class="text-center" id="hide_div">We strive to keep class sizes small and maintain an excellent Teaching Artist/student ratio. For these reasons, we are unable to offer make up classes.</p>
+				<h3 class="text-center click_me">Are make up classes available?</h3>
+				<p class="text-center hide_div">We strive to keep class sizes small and maintain an excellent Teaching Artist/student ratio. For these reasons, we are unable to offer make up classes.</p>
 			</div>
 		</div>
 	</div>
@@ -150,8 +150,8 @@
 	<div class="container">
 		<div class="row">
 			<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-				<h3 class="text-center" id="click_me">Absences, late arrivals, and withdraws:</h3>
-				<p class="text-center" id="hide_div">Families are expected to bring students to class on time. Please inform your child’s Teaching Artist in advance of any necessary absences during the session when possible, especially if your student will not be able to attend the last day of class during the Sharing or Showcase.</p>
+				<h3 class="text-center click_me">Absences, late arrivals, and withdraws:</h3>
+				<p class="text-center hide_div">Families are expected to bring students to class on time. Please inform your child’s Teaching Artist in advance of any necessary absences during the session when possible, especially if your student will not be able to attend the last day of class during the Sharing or Showcase.</p>
 			</div>
 		</div>
 	</div>

047caf66d392793a8a255e93c38bc77950539929
rocketpastsix <mjftrask@gmail.com>
2014-10-06 07:38:12 -0400

    added content for styles

--- schedule.php
+++ schedule.php
@@ -1,2 +1,23 @@
 <?php include('inc/header.php'); ?>
+
+<main class="body">
+
+	<div class="container">
+		<div class="row">
+			<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+				<h1 class="text-center">Class Schedule for Fall 2014</h1>
+				<h4 class="text-center">Classes at Off Broadway  run the same calendar as the school year, beginning in August and ending with a recital in May. Open enrollment is possible year-round if there is space in a particular class. Class sizes are limited to ensure proper attention can be given to each student.</h4>
+			</div>
+		</div>
+	</div>
+
+
+
+
+
+
+
+
+</main>
+
 <?php include('inc/footer.php'); ?>

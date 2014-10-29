7d704144c22aed030bdb3e41d18b869da5c57193
rocketpastsix <mjftrask@gmail.com>
2014-10-28 22:23:40 -0400

    all sorts of fun

---
+++ whatsnew.php
@@ -0,0 +1,55 @@
+<?php include('inc/header.php'); ?>
+
+<main class="body">
+	<div class="container">
+		<div class="row">
+			<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+				<h1 class="text-center">What's new at the theatre!</h1><hr />
+			</div>
+		</div>
+	</div>
+
+	<div class="container">
+		<div class="row">
+			<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+				<p class="text-center">The Off Broadway Children’s Theatre is pleased to announce auditions for its elite company, 2014-2015 Off Broadway Kids Musical Theatre Troupe, the show for the year is “Suessical Jr.”. The auditions are open to kids ages 8-17 and will be held Saturday, August 16, 2014 from 2pm-5pm at the studios of Off Broadway Dance Theatre in Alpharetta Georgia. Auditionees should prepare 18 measures of a musical theatre song with accompaniment (no acapella auditions are allowed), dress comfortably for a movement audition and be prepared to cold read from the script.
+									   <br /><br />Directed by Christian Mayer, "The OBD Kids" is our award-winning musical theatre competition troupe designed for serious "triple threat" performers who live and breathe the theatre. Each year a small cast of performers, ranging in age from 8 - 18, are chosen to be members of the Troupe. After a lengthy audition process, the OBD Kids rehearse their show an average of 4 hours a week over a 7 month period, in addition to polishing their acting, dance and vocal skills via weekly lessons. Most troupe members put in an average of 8-10 hours in training each week, but their hard work pays off in terms of a wonderful camaraderie among cast members, an exciting full production in our blackbox theatre, performances at local festivals and charity events, and the thrill of competing with other theatre and drama teams across the United States. Alumni of the 12 year program, can be found appearing as the top performers at Milton High School, West Forsyth High School, Pebblebrook Performing Arts High School, North Springs Performing Arts High School as well as many prestigious Theatre programs in Universities throughout the United States. There will be a yearly participation fee of $750 as well as a commitment to take 1 weekly class of training at OBD if chosen.</p> 
+				<p class="text-center">This troupe travels to The National Performing Arts Festival in Walt Disney World each year to compete against theatres from around the nation. Call today to schedule an audition appointment at 770-664-2410</p><hr />
+			</div>
+		</div>
+	</div>
+
+	<div class="container" style="margin-top: 20px;">
+		<div class="row">
+			<div class="col-xs-12 col-sm-6 col-md-6 col-lg-5">
+				<img src="img/anniejr.jpg">
+			</div>
+			<div class="col-xs-12 col-sm-6 col-md-6 col-lg-7">
+				<p class="text-center">With audtions concluded, we are now in the middle of preparing for our next preformance, Annie Jr!</p>
+				<p class="text-center">Please call, email or stop by the theatre to get tickets to one of our shows. December 12-21</p>
+				<p class="text-center">It makes for a perfect holiday date night!</p>
+				<p class="text-center">Tickets on sale November 15th!</p>
+			</div>
+		</div>
+	</div>
+
+	<div class="container" style="margin-top: 20px;">
+		<div class="row">
+			<div class="col-xs-12 col-sm-6 col-md-6 col-lg-6">
+				<p class="text-center">Check out our Sweet Apple Elementary School Friends perform The Jungle Book!</p>
+				<p class="text-center">November 7th and November 8th, only at Off Broadway Theatre!</p>
+				<p class="text-center">Tickets on sale now!</p>
+			</div>
+			<div class="col-xs-12 col-sm-6 col-md-6 col-lg-6">
+				<img src="img/junglebook.jpg">
+			</div>
+		</div>
+	</div>
+</main>
+
+<?php include('inc/footer.php'); ?>
+
+
+
+
+

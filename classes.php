2b5aaf47b35f767a5fb9ad7e86216a287798da69
rocketpastsix <mjftrask@gmail.com>
2015-01-07 18:08:55 -0500

    finally fixed gitignore. framework for classes.php

--- classes.php
+++ classes.php
@@ -5,6 +5,7 @@
 	<div class="container">
 		<div class="row">
 			<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+				<h1 class="text-center">Classes that we offer at Off Broadway Performence Center</h1>
 			</div>
 		</div>
 	</div>

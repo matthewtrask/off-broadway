e33df1e3fa22e918374f372fabb3e05e2183c20d
matt trask <mjftrask@gmail.com>
2015-02-16 21:15:26 -0500

    updates to css for the footer

--- public/inc/footer.php
+++ public/inc/footer.php
@@ -2,18 +2,18 @@
 		<div class="container">
 			<div class="row">
 				<div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
-					<p class="text-center">Off Broadway Dance Studio</p>
-					<p class="text-center">12350 Crabapple Rd, Ste. 122</p>
-					<p class="text-center">Alpharetta GA, 30049</p>
-					<p class="text-center">770-664-2410</p>
+					<p class="text-left">Off Broadway Dance Studio</p>
+					<p class="text-left">12350 Crabapple Rd, Ste. 122</p>
+					<p class="text-left">Alpharetta GA, 30049</p>
+					<p class="text-left">770-664-2410</p>
 				</div>
 				<div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
 					<p class="text-center" id="copy">&copy; <?php echo date("Y"); ?> <?php echo $studio; ?></p>
 					<p class="text-center">Site Visitors: <?php include('counter.php'); ?></p>
 				</div>
 				<div class="col-xs-4 col-sm-4 col-md-4 col-lg-4" id="socialmedia">
-					<p class="text-center">Where we are on the web:</p>
-					<ul class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+				<p class="text-center">Where we are on the web:</p>
+				<ul class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
 						<li class="text-left" id="1"><a href="https://www.facebook.com/offbroadwaydancetheater">Off Broadway Facebook</a></li>
 						<li class="text-left" id="2"><a href="https://twitter.com/offbroadwayga">@Offbroadway</a></li>
 						<li class="text-left" id="3"><a href="https://offbroadwaydance.wordpress.com">Shannon's Blog</a></li>
@@ -41,7 +41,7 @@ $(document).ready(function(){
     ga('send', 'pageview');
 </script>
 
-// <script>
+<script>
 //   (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
 //   (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
 //   m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)

a7ee0931be43d33c0c63c14c3836166220553c20
matt trask <mjftrask@gmail.com>
2015-02-18 22:32:01 -0500

    base of form for registration started

--- public/reg.php
+++ public/reg.php
@@ -6,8 +6,13 @@ include('inc/header.php');
 	<div class="row">
 		<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
 			<p>Use this form to register your child for theater and dance classes</p>
-			<form>
+			<p><em>Contact Information</em></p>
+			<form class="form-inline">
 				<div class="form-group">
+					<label for="parentFirstName">Parent's First Name</label>
+					<input type="parentFirstName" class="form-control" id="parentFirstName" placeholder="Parent's First Name">
+					<label for="parentLastName">Parent's Last Name</label>
+					<input type="parentLastName" class="form-control" id="parentLastName" placeholder="Parent's Last Name"> 
 				</div>
 			</form>
 		</div>

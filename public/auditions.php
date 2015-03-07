519b381b1ee7b9f42ccc8c67c3e1e39e6390ab35
matt trask <mjftrask@gmail.com>
2015-03-07 16:00:28 -0500

    updated the auditions page with new content

--- public/auditions.php
+++ public/auditions.php
@@ -12,6 +12,20 @@
 
 	<div class="container">
 		<div class="row">
+			<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+				<h2 class="text-center">OBCT Apprentice Theater Troupe 2015-2016</h2>
+				<p class="text-left"><b>What</b>: A group of 20-25  selected by audition performers between the ages of 6-12 that are dedicated and want to take their theater experience to the next level.</p>
+				<p class="text-left"><b>Where</b>: The Off Broadway Childrens Theater in Alpharetta, Georgia</p>
+				<p class="text-left"><b>When</b>: They will audition in May 2015 and then will participate in a 2 hour Dance/Improv/Acting class each Saturday from August to January. They will rehearse a Broadway Jr. show either “Dear Edwina Jr.” or “Captain Louie Jr.” on Saturday mornings that will be performed in January 2016. They will attend and compete with a 15 minute selection of the show at the 2016 Junior Theater Festival (JTF) in Atlanta, Georgia.</p>
+				<p class="text-left"><b>Why</b>: The Off Broadway Kids Theater Troupe has thrived since 2002 and the demand for serious training for younger performers that can mature into the senior group has been overwhelming.</p>
+				<p class="text-left"><b>How</b>: The audition date is May 9, 2015  from 2:00-5:00 PM  and May 13, 2015 from 6:30-8:30 PM and audition slots can be booked by emailing <a href="mailto:Offbroadway@msn.com?subject=OBD Troupe Information">offbroadway@msn.com</a> or calling 770-664-2410. The cost of the troupe is $350.00 and the monthly cost for the instruction is $90.00 per month. The cost of JTF is separate and will be determined once the information becomes available. The troupe number is limited. Troupe members will have discounted production fees for other shows if they choose through March of 2016 for all shows OBCT offers.Troupe members will need to attend a 2 day summer intensive on June 22-23, 2015 from 2:00-6:00 PM. Cost of the intensive is $50.00 dollars.</p>
+				<p class="text-left">**Admission into the Off Broadway Kids Senior Troupe is open to kids by invited audition only for anyone ages 8 - 10, and 11 and older by general open audition. The number of open slots for younger Senior Troupe members this year will be very limited.</p>
+			</div>
+		</div><hr />
+	</div>
+
+	<div class="container">
+		<div class="row">
 			<div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
 				<h2 class="text-center">Frequently asked questions about the audition process</h2><hr />
 				<h3 class="text-center click_me">Are actors paid?</h3>

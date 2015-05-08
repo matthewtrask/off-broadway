d3db5a34b8d813030f07fb513bd349abf63cd897
matt trask <mjftrask@gmail.com>
2015-05-07 21:59:09 -0400

    changed formatting

--- public/teachers.php
+++ public/teachers.php
@@ -7,13 +7,13 @@
 			<hr />
 			<h1 class="text-center">Our Connectors</h1>
 			<hr/>
-			<div class="col-xs-12 col-sm-12 col-md-6 col-lg-6">
+			<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
 				<img src="img/shannon_mayer.jpg" alt="" class="img-responsive center-block">
 				<h4 class="text-center">Shannon Mayer</h4>
 				<p class="text-center">Shannon Hannah Mayer became Owner/Director of Off Broadway Dance Center in June of 2000. A native of Dothan, Alabama, she began her training in dance at the early age of 8 years old. Shannon received a Bachelor of Arts in Theatre from Brenau Women's College in Gainesville, Georgia. She went on to pursue a career in musical theatre and dance which has given her the opportunity to perform throughout the southeastern United States.</p>
 				<p class="text-center hide_text">Shannon has appeared as a performer for Discovery Cruise Lines, as a performer and Dance Captain for the Savoyards Musical Theatre Company and is the resident choreographer for many north Georgia theatre companies as well as remaining an active performer. She is dedicated to the education of children and the commitment to helping the community.</p>
 			</div>
-			<div class="col-xs-12 col-sm-12 col-md-6 col-lg-6">
+			<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
 				<img src="img/chris_shannon.jpg" alt="" class="img-responsive center-block">
 				<h4 class="text-center">Chris Mayer</h4>
 				<p class="text-center">Christian Mayer is Artistic Director and Owner of the Off Broadway Dance and Children's Theatre in Alpharetta, Ga. He has produced and directed over 50 children's productions and has mentored, coached and groomed hundreds of kids. He began his acting career in 1981, and has performed in such notable shows as "House of Blue Leaves", "Two for the Seesaw", "Bent", "Hatful of Rain", "Homefront", "The Mouestrap" "The Elephant Man","Loot" and was lucky enough to have proposed to his wife onstage in "Annie". Christian believes that theatre is a journey and that theatre is about the work of the whole, not the individual.</p>
@@ -23,18 +23,12 @@
 	</div>
 	<div class="container" id="bios">
 		<div class="row">
-			<div class="col-xs-12 col-sm-12 col-md-4 col-lg-4">
+			<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
 				<img src="img/erin_obd.jpg" alt="" class="img-responsive center-block">
 				<h4 class="text-center">Erin Greenway</h4>
 				<p class="text-center">Erin Greenway studied acting, dance, choreography, and photography in the States and abroad. While in London, she had an apprenticeship with the English National Ballet.  While with the Ballet she participated in the Community Education program exposing elementary schools, elderly groups, and children's hospitals to the world of ballet.  After moving back to the states, she helped to set up Greensboro, North Carolina's first Arts Advocacy Council, and taught dance to elementary school children with learning disabilities.  Companies that Erin has worked with include Spitzer Dance Company, Offshoot Productions, Lee Harper and Dancers, Stage Door Players, Ansley Park Playhouse, Jib Jam Productions, Odd Man Out Theatre, Rosewater Theatre, Performing Arts North Theatre Decatur, Academy Theatre, and extensively with Savage Tree Arts Project.  Erin is also the Chair of Fine Arts department at the Brandon Hall School and is an company member of Performing Arts North Repertory Company.</p>
 			</div>
-			<div class="col-xs-12 col-sm-12 col-md-4 col-lg-4">
-				<img src="img/lauren.jpg" alt="" class="img-responsive center-block">
-				<h4 class="text-center">Lauren Overstreet</h4>
-				<p class="text-center">Lauren studied and trained in dance from age 3 at Susan Chamber School of Dance in Alpharetta, GA. She attended numerous conventions and competitions growing up and has trained with numerous established instructors such as Mia Michaels, Mike Minery, Kris Cangelosi, Ellen Tshudy, Susan Anderson, and Michael Garrison. She has been  a member and choreographer of Xtensity Atlanta, Dancefx Concert Dance Company, and Pamoja Dance Company at UGA for several years.</p>
-				<p class="text-center hide_text">Lauren is currently a member of Project 7 Dance Company in Roswell, GA. More recently, she has been the music video choreographer for both Emergent Heart's "Top Shelf" and Pacific UV's "Funny Girl", and she was a choreographer for Dancefx Concert Dance Company's "Alice : Lessons in Wonderland". She also recently choreographed and staged the opening number for the annual Flagpole awards in Athens, GA.  In 2012, she graduated from the University of Georgia with a Bachelors of Science degree in Geology.</p>
-			</div>
-			<div class="col-xs-12 col-sm-12 col-md-4 col-lg-4">
+			<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
 				<img src="img/AmyNoelWelch.jpg" class="img-responsive center-block">
 				<h4 class="text-center">Amy Noel Welch</h4>
 				<p class="text-center">Amy has been teaching and playing in the Atlanta area for many years. She attended Georgia State University, majoring in music education and is proficient on trumpet and piano, as well as voice. Being the music director for Elm Street Cultural Arts Village in Woodstock since 2009, she has worked on over 30 shows and is in high demand as an accompanist in Cherokee County schools and the community.</p>

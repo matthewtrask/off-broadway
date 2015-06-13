6e3e181831b4ac0d2b614dc09908f1904781068c
matt trask <mjftrask@gmail.com>
2015-06-13 11:45:13 -0400

    rebuilding site, starting off with controller

---
+++ public/V2/app/Controllers/OBD.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace Controllers;
+
+use Core\View;
+use Core\Controller;
+
+
+class OBD extends Controller{
+
+  public function index() {
+    $data['title'] = 'Home';
+
+    View::rendertemplate('header', $data);
+    View::rendertemplate('home');
+    View::rendertemplate('footer');
+  }
+
+  public function WhatsNew() {
+
+  }
+
+  public function Schools() {
+
+  }
+
+  public function AboutUs () {
+
+  }
+
+  public function Teachers() {
+
+  }
+
+  public function Troupe() {
+
+  }
+
+  public function Classes() {
+
+  }
+
+  public function Questions() {
+
+  }
+
+  public function CurrentProd() {
+
+  }
+
+  public function Upcoming() {
+
+  }
+
+  public function Auditions() {
+
+  }
+
+  public function Register() {
+
+  }
+
+  public function BoxOffice() {
+
+  }
+
+  public function Contact(){
+
+  }
+}
+?>

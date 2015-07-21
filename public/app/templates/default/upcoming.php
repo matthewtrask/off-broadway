ed912da17b7adcfb76f0f4e71a7320f526d9f31f
matt trask <mjftrask@gmail.com>
2015-07-20 23:11:24 -0400

    fixed the upcoming events

--- public/app/templates/default/upcoming.php
+++ public/app/templates/default/upcoming.php
@@ -2,40 +2,28 @@
   <div class="row">
     <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
       <h1 class="text-center">Our Upcoming Shows!</h1>
-      <h3 class="text-center">Check here often to see what we are planning for you!</h3><hr />
+      <h3 class="text-center">Check here often to see what we are planning for you!</h3>
     </div>
-  </div>
+  </div><hr>
 </div>
-
 <div class="container">
   <div class="row">
     <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-      <h2 class="text-center">We are proud to announce our next productions!</h2>
-      <h3 class="text-center">Click <a href="/boxoffice.php">here</a> for information on how to buy tickets!</h3>
+      <h2 class="text-center">We are currently auditioning for our Senior Troupe.</h2>
+      <h4 class="text-center">Check our <a href="troupe">troupe</a> page for more information or email us at <a href="mailto:offbroadway@msn.com">offbroadway@msn.com</a> for more details!</h4>
     </div>
-  </div>
-</div>
+  </div><hr>
+</div> 
 
-<div class="container" style="margin-top: 30px">
+<div class="container">
   <div class="row">
     <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
-      <h2 class="text-center">Little Mermaid Jr.</h2>
-      <p class="text-center">August 7th - August 16th</p>
+      <h2 class="text-center">We are currently auditioning for Cinderella Kids!</h2>
+      <h4 class="text-center">For more information or email us at <a href="mailto:offbroadway@msn.com">offbroadway@msn.com</a> for more details!</h4>
     </div>
     <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
-      <img src="<?php echo IMGDIR; ?>littlemermaid.jpg" height="300px" width="300px">
+      <img src="<?php echo IMGDIR; ?>cinderella.jpg" alt="Cinderella Kids" class="img-responsive center-block">
     </div>
   </div>
-</div>
+</div> 
 
-<div class="container" style="margin-top: 30px">
-  <div class="row">
-    <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
-      <h2 class="text-center">Jungle Book Kids</h2>
-      <p class="text-center">July 17th - July 18th</p>
-    </div>
-    <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
-      <img src="<?php echo IMGDIR; ?>jb.jpg" height="300px" width="300px">
-    </div>
-  </div>
-</div>

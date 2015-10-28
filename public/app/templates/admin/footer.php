63bf03b378fb1d81f4b297f24b7e2df68d9f1c00
matt trask <mjftrask@gmail.com>
2015-10-27 23:10:38 -0400

    added foundation js

--- public/app/templates/admin/footer.php
+++ public/app/templates/admin/footer.php
@@ -8,13 +8,13 @@ use Helpers\Url;
 ?>
 
 
-
+</main>
 <!-- JS -->
 <script src="/bower_components/foundation/js/foundation.js"></script>
 <script src="/bower_components/foundation/js/foundation/foundation.dropdown.js"></script>
-<script src="/bower_components/foundation/js/foundation/foundation.accordian.js"></script>
 <script src="/bower_components/foundation/js/foundation/foundation.topbar.js"></script>
 <script src="/bower_components/foundation/js/foundation/foundation.alert.js"></script>
+<script src="/bower_components/foundation/js/foundation/foundation.reveal.js"></script>
 <script src="/bower_components/foundation/js/foundation/foundation.abide.js"></script>
 <!-- <script src='../bower_components/flowtype/flowtype.js'></script>
  -->
